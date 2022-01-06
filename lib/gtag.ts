0b0e918a7f1184a308bb137fc264319e90bdab1b
appare45 <64473501+appare45@users.noreply.github.com>
2022-01-07 00:34:26 +0900

    updated gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.GTAG_TRACKING_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GTAG_TRACKING_ID;
 
 export const pageView = (url: string) => {
   (window as any).gtag("config", GA_TRACKING_ID, {
