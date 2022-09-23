ce28eb6e02eca044651b0fa931ce6b4cbafb2dda
appare <64473501+appare45@users.noreply.github.com>
2022-09-23 15:15:22 +0900

    added hrecaptcha test

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,11 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+const hCaptchaVerifyUrl = "https://hcaptcha.com/siteverify";
 
 const allowedHost =
   process.env.NODE_ENV == "development"
     ? ["localhost", "0.0.0.0"]
     : ["appare45.com", "blog-appare45.vercel.app"];
 
-export default function handler(
+export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
@@ -14,10 +17,24 @@ export default function handler(
     allowedHost.includes(new URL(request.headers.referer ?? "").hostname) &&
     request.headers["content-type"] == "application/x-www-form-urlencoded" &&
     request.body["h-captcha-response"] &&
-    request.body["g-recaptcha-response"]
+    request.body["g-recaptcha-response"] &&
+    request.body["text"]
   ) {
-    console.info(request.body);
-    response.redirect("/contact/success");
+    const hCaptchaRequestBody = {
+      secret: process.env.HCAPTCHA_API_KEY,
+      response: request.body["h-captcha-response"],
+    };
+    const hCaptchaData = await axios.post(
+      hCaptchaVerifyUrl,
+      hCaptchaRequestBody,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    console.log(hCaptchaData);
+    if (hCaptchaData.status == 202 && JSON.parse(hCaptchaData.data)["success"])
+      response.redirect("/contact/success");
+    else response.redirect("/contact/fail");
   } else {
     response.redirect("/contact/fail");
   }
